372f30c105c7b29a566ac4bcb45ebbabec1a6755
naokiurasawa <drugsunduk8@gmail.com>
2024-08-05 19:27:30 +0300

    Add files via upload

---
+++ triangleHometask.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void drawTriangle(int n) {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << '*';
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	drawTriangle(n);
+}
